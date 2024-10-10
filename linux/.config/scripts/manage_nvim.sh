2f48d359306ff3d17c9c9fc12897e5c2437c92a1
D4RKAR117 <D4RKAR117@outlook.es>
2024-10-09 20:01:04 -0500

    feat(lazygit): added lazygit management

--- linux/.config/scripts/manage_nvim.sh
+++ linux/.config/scripts/manage_nvim.sh
@@ -63,3 +63,34 @@ if version_gt "$latest_starship_version" "$current_starship_version"; then
 else
   echo "Starship is already up-to-date (version $current_starship_version)."
 fi
+
+
+if command -v lazygit &> /dev/null; then
+  # Get the current installed version of Lazygit looking for first "version=0.0.0" on the output
+  # the output string ignoring "git version=0.0.0" due to beign present afterwards
+  current_lazygit_version=$(lazygit --version  | grep -oP 'version=\K[^ ]+' | head -n 1)
+  # trim any , 
+  current_lazygit_version="${current_lazygit_version//,}"
+else
+  current_lazygit_version="0.0.0"
+fi
+
+latest_lazygit_version=$(curl -s https://api.github.com/repos/jesseduffield/lazygit/releases/latest | grep -Po '"tag_name": "v\K[^"]*')
+
+if version_gt "$latest_lazygit_version" "$current_lazygit_version"; then
+    echo "Updating Lazygit from version $current_lazygit_version to $latest_lazygit_version..."
+    # Download the latest Lazygit release
+    curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${latest_lazygit_version}_Linux_x86_64.tar.gz"
+    tar xzf lazygit.tar.gz lazygit
+
+    # Remove the old Lazygit directory
+    sudo rm -rf /usr/local/bin/lazygit
+
+    sudo install lazygit /usr/local/bin
+
+    # Clean up
+    rm -rf lazygit.tar.gz lazygit
+
+else
+  echo "Lazygit is already up-to-date (version $current_lazygit_version)."
+fi
